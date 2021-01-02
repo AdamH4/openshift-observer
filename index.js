d2897c35614f2f9c9ae02c42c7c5da8376c79677
Adam Harnusek <adam.harnusek@gmail.com>
2021-01-02 15:26:53 +0100

    Remove this pod :finaly

--- index.js
+++ index.js
@@ -44,14 +44,14 @@ function filterPodEnvVariables(){
     let enviromentOpenshiftVariables = {}
     Object.keys(process.env).forEach(key => {
         const envKey = String(key)
-        if(envKey.endsWith("SERVICE_PORT") && !(envKey.includes("KUBERNETES") || envKey.includes("OPENSHIFT-OBSERVER"))){
+        if(envKey.endsWith("SERVICE_PORT") && !(envKey.includes("KUBERNETES") || envKey.includes("OPENSHIFT_OBSERVER"))){
             const keyIndex = envKey.indexOf("SERVICE_PORT")
             const key = envKey.slice(0,keyIndex - 1)
             if(enviromentOpenshiftVariables[key] === undefined){
                 enviromentOpenshiftVariables[key] = {}
             }
             enviromentOpenshiftVariables[key].port = process.env[envKey]
-        }else if(envKey.endsWith("SERVICE_HOST") && !(envKey.includes("KUBERNETES") || envKey.includes("OPENSHIFT-OBSERVER"))){
+        }else if(envKey.endsWith("SERVICE_HOST") && !(envKey.includes("KUBERNETES") || envKey.includes("OPENSHIFT_OBSERVER"))){
             const keyIndex = envKey.indexOf("SERVICE_HOST")
             const key = envKey.slice(0,keyIndex - 1)
             if(enviromentOpenshiftVariables[key] === undefined){
