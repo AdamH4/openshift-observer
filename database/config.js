c7b02b473efc5bbeeb7cd51af3863a6196c3af17
Adam Harnusek <adam.harnusek@gmail.com>
2021-03-02 08:27:17 +0100

    Log knexfile

--- database/config.js
+++ database/config.js
@@ -3,6 +3,7 @@ const knexfile = require('../knexfile');
 
 // const env = process.env.NODE_ENV || 'development';
 
+console.log(knexfile['openshift'])
 const configOptions = knexfile['openshift'];
 
 module.exports = knex(configOptions);
