f170a7826c2befd495ab97d16e8c3ad6b170c8ce
Adam Harnusek <adam.harnusek@gmail.com>
2021-05-04 13:10:30 +0200

    Remove unused funcitons for parsing

--- helpers.js
+++ helpers.js
@@ -1,12 +1,3 @@
-// find pod with same name and return it's index
-const findPodIndexByBuildName = (pods, buildName) => {
-    const lookingPodName = buildName.slice(0, buildName.lastIndexOf("-"))
-    let podIndex = undefined
-    pods.forEach((pod, index) => {
-        if (pod.name === lookingPodName) podIndex = index
-    })
-    return podIndex
-}
 
 // get build repo URL and return it defaults to empty string ("")
 const findBuildRepoURL = (build) => {
@@ -19,62 +10,6 @@ const findBuildRepoURL = (build) => {
         }
     })
     return URL
-}
-
-//get builds from response and update pods
-const parseBuildAndUpdatePods = (items, pods) => {
-    items.forEach(item => {
-        if (item.metadata.ownerReferences[0].kind === "Build") {
-            const podIndex = findPodIndexByBuildName(pods, item.metadata.labels["openshift.io/build.name"])
-            if (typeof podIndex !== undefined) {
-                pods[podIndex].buildsCount++
-                pods[podIndex].repoLink = pods[podIndex].repoLink ? pods[podIndex].repoLink : findBuildRepoURL(item)
-            }
-        }
-    })
-    return pods
-}
-
-// loop pod array and return index of pod with podName
-// or return undefined
-const checkIfPodIsListed = (pods, podName) => {
-    let i = 0
-    for (let pod in pods) {
-        if (pod.name === podName) return i
-        i++
-    }
-    return undefined
-}
-
-//get just pods from response
-const parsePods = (items) => {
-    let pods = []
-    items.forEach(item => {
-        if (item.metadata.ownerReferences[0].kind === "ReplicaSet") {
-            const podIndex = checkIfPodIsListed(pods, item.metadata.labels.app)
-            if (podIndex === undefined) {
-                pods.push({
-                    name: item.metadata.labels.app,
-                    cluster: item.metadata.namespace,
-                    creationTimestamp: item.metadata.creationTimestamp,
-                    status: item.status.phase,
-                    podLink: `http://${item.metadata.labels.app}-${item.metadata.namespace}.apps-crc.testing`,
-                    containers: item.spec.containers.map(container => {
-                        return {
-                            containerName: container.name,
-                            ports: container.ports
-                        }
-                    }),
-                    replicaSetCount: 0,
-                    repoLink: 0,
-                    buildsCount: null,
-                })
-            } else {
-                pods[podIndex].replicaSetCount++
-            }
-        }
-    })
-    return pods
 }
 
 // parse data from build
