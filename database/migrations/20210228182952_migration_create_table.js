825ad61e0b99db516d43353934797d9cbbd4d918
Adam Harnusek <adam.harnusek@gmail.com>
2021-05-04 12:06:45 +0200

    Add new tables

--- database/migrations/20210228182952_migration_create_table.js
+++ database/migrations/20210228182952_migration_create_table.js
@@ -1,18 +1,39 @@
 
 exports.up = function (knex) {
   return knex.schema.createTable("pods", table => {
-    table.increments("id").primary()
+    table.text("uid").primary()
     table.text("name").notNullable().unique()
     table.text("url")
     table.text("cluster_name")
     table.text("status_message")
+    table.timestamp("creation_timestamp")
     table.text("repository_link")
-    table.text("replicaset_count")
-    table.text("build_count")
+    table.integer("replicaset_count")
     table.json("oas")
   })
+    .createTable("builds", table => {
+      table.text("uid").primary()
+      table.text("pod_name").references("name").inTable("pods")
+      table.text("build_source")
+      table.integer("build_order")
+    })
+    .createTable("containers", table => {
+      table.increments("id").primary()
+      table.text("pod_uid").references("name").inTable("pods")
+      table.text("build_source")
+      table.integer("build_order")
+    })
+    .createTable("ports", table => {
+      table.increments("id").primary()
+      table.integer("container_id").references("id").inTable("containers")
+      table.integer("port")
+      table.text("protocol_name")
+    })
 }
 
 exports.down = function (knex) {
   return knex.schema.dropTableIfExists("pods")
+    .dropTableIfExists("builds")
+    .dropTableIfExists("containers")
+    .dropTableIfExists("ports")
 }
