30e98c2f82505ac6ec85dc258ea1298bcb320a34
Adam Harnusek <adam.harnusek@gmail.com>
2021-05-11 10:21:50 +0200

    Rollback DB before initilization

--- index.js
+++ index.js
@@ -124,6 +124,7 @@ app.listen(port, async function () {
     let retries = 5
     while (retries) {
         try {
+            await knex.migrate.rollback()
             await knex.migrate.latest()
             watchPods()
             break
