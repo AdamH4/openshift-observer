7a4b4fe4382e205d236094f0e8ec3459a80d5130
Adam Harnusek <adam.harnusek@gmail.com>
2021-05-11 10:19:32 +0200

    Only one DB config

--- knexfile.js
+++ knexfile.js
@@ -2,25 +2,25 @@ require('dotenv').config()
 
 module.exports = {
 
-    development: {
-        client: 'pg',
-        // connection: process.env.DATABASE_URL,
-        connection: {
-            host: process.env.DEV_DB_HOST,
-            user: process.env.DEV_DB_USER,
-            database: process.env.DEV_DB_NAME,
-            password: process.env.DEV_DB_PASSWORD,
-            port: 5432
-        },
-        migrations: {
-            directory: './database/migrations'
-        },
-        seeds: {
-            directory: './database/seeds'
-        }
-    },
+    // development: {
+    //     client: 'pg',
+    //     // connection: process.env.DATABASE_URL,
+    //     connection: {
+    //         host: process.env.DEV_DB_HOST,
+    //         user: process.env.DEV_DB_USER,
+    //         database: process.env.DEV_DB_NAME,
+    //         password: process.env.DEV_DB_PASSWORD,
+    //         port: 5432
+    //     },
+    //     migrations: {
+    //         directory: './database/migrations'
+    //     },
+    //     seeds: {
+    //         directory: './database/seeds'
+    //     }
+    // },
 
-    openshift: {
+    development: {
         client: 'pg',
         connection: {
             host: process.env.POSTGRESQL_SERVICE_HOST || process.env.HOST,
