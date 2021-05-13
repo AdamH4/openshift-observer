a93f7368c59956557cc96126710809df633c2334
Adam Harnusek <adam.harnusek@gmail.com>
2021-05-13 14:24:48 +0200

    Right scope

--- database/config.js
+++ database/config.js
@@ -4,6 +4,6 @@ const knexfile = require('../knexfile')
 // const env = process.env.NODE_ENV || 'development';
 
 // console.log(knexfile['openshift'])
-const configOptions = knexfile['development']
+const configOptions = knexfile['openshift']
 
 module.exports = knex(configOptions)
